816264c21646ac714cab437e2e31c3bbc6d4a84d
Emmanuel Balogun <emanuelbalogun@gmail.com>
2023-08-25 23:21:10 -0500

    Add Navigation Bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,16 @@ import React from "react";
 
 import PhotoListItem from "./components/PhotoListItem";
 import "./App.scss";
-
+import TopNavigation from "components/TopNavigationBar";
 import PhotoList from "components/PhotoList";
-import TopicList from "components/TopicList";
+
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   
   return (
     <div className="App">
-      <TopicList/>
+      <TopNavigation/>
       <PhotoList/>
       {/* <PhotoListItem
         imagesource = {sampleDataForPhotoListItem.imageSource}
